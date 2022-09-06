71b1da081fc42c9c58d4636eed85d7aacc92602d
Emil <emil88pl@gmail.com>
2022-09-06 16:46:31 +0100

    init

---
+++ src/main/java/com/qa/accountexample/AccountexampleApplication.java
@@ -0,0 +1,13 @@
+package com.qa.accountexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountexampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qa/accountexample/domain/Account.java
@@ -0,0 +1,58 @@
+package com.qa.accountexample.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String accountNumber;
+
+    @Column
+    private String field;
+
+    public Account(long id, String accountNumber, String field) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.field = field;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+
+    // A String accountNumber field that is both unique and not null.
+
+    // A Long id field that is set to AUTO_INCREMENT.
+
+    // A String name field.
+}
